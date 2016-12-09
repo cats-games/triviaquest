96494d4b4571432e5b680e02f6de95af0ed0cc79
Cai Lu <caillu@Adorable.local>
2016-12-09 15:48:43 -0800

    Connect server to database

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,24 +1,11 @@
 // require the challenge model
 var Promise = require('bluebird');
-//var Challenge = Promise.promisify(require('models/challenge'));
+var Challenge = Promise.promisifyAll(require('./models/challenge'));
 
 
 // Gets all challenges from the database
 exports.getChallenges = function(request, response) {
-  /*
-  in mongoose
   Challenge.find({}).then(function (challenges) {
     response.json(challenges);
   });
-  */
-  // For testing:
-  response.json([{
-    prompt: 'This is a prompt',
-    solution: 'This is the solution to the prompt',
-    level: 2
-  }, {
-    prompt: 'This is another prompt',
-    solution: 'This is the solution to the prompt',
-    level: 1
-  }]);
 };
