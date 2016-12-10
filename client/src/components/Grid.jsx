d9300ab8b4c1f78dcfab7c3cb6c4ef347ea70b3c
Cai Lu <caillu@Adorable.local>
2016-12-10 11:22:56 -0800

    Render enemies on grid

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -3,8 +3,11 @@ class Grid extends React.Component {
     super(props);
 
     this.state = {
-      mapArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+      mapArray: {},
+      challenges: []
     };
+
+    this.spaces = 25;
   }
 
   componentWillMount() {
@@ -20,6 +23,41 @@ class Grid extends React.Component {
       .fail(function(error) {
         console.error('Could not get challenges:', error);
       });
+
+    for (var i = 1; i <= this.spaces; i++) {
+      this.state.mapArray[i] = {
+        hasEnemy: false,
+        id: i
+      };
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    // Check if challenges exist
+    if (this.state.challenges.length === 0 && nextState.challenges.length) {
+      // Populate the mapArray with enemies
+      var enemySquares = _.range(1, this.spaces + 1);
+      enemySquares.splice(this.state.activeNumber - 1, 1);
+
+      this.shuffle(enemySquares);
+
+      enemySquares = enemySquares.slice(0, nextState.challenges.length);
+
+      enemySquares.forEach(function(enemy) {
+        this.state.mapArray[enemy]['hasEnemy'] = true;
+      }.bind(this));
+
+    }
+  }
+
+  shuffle(array) {
+    console.log(array);
+    for (var i = array.length - 1; i > 0; i--) { 
+      var j = Math.floor(Math.random() * (i + 1)); 
+      var temp = array[i]; 
+      array[i] = array[j]; 
+      array[j] = temp; 
+    } 
   }
 
   componentWillUnmount() {
@@ -33,32 +71,33 @@ class Grid extends React.Component {
     var rows = Math.sqrt(this.state.mapArray.length);
     if (e.which === 72) {
       // h / move left
-      if ((this.state.activeNumber - 1) % 5 !== 0) {
+      if ((this.state.activeNumber - 1) % rows !== 0) {
         this.setState({activeNumber: this.state.activeNumber - 1});
       }
     } else if (e.which === 74) {
       // j / move down
       if (this.state.activeNumber <= rows * (rows - 1)) {
-        this.setState({activeNumber: this.state.activeNumber + 5});
+        this.setState({activeNumber: this.state.activeNumber + rows});
       }
     } else if (e.which === 75) {
       // k / move up
       if (this.state.activeNumber > rows) {
-        this.setState({activeNumber: this.state.activeNumber - 5});
+        this.setState({activeNumber: this.state.activeNumber - rows});
       }
     } else if (e.which === 76) {
       // l/ move right
-      if (this.state.activeNumber % 5 !== 0) {
+      if (this.state.activeNumber % rows !== 0) {
         this.setState({activeNumber: this.state.activeNumber + 1});
       }
     }
   }
 
   render() {
+
     var gridNumber = 1;
     return (
       <div id="grid" onKeyDown={this.setActiveNumber}>
-      {this.state.mapArray.map((abox) => <div className={"gridbox " + (this.state.activeNumber === (gridNumber) ? "activated" : "")} id={gridNumber++}>{abox}</div>
+      {Object.keys(this.state.mapArray).map((abox) => <div className={"gridbox " + (this.state.activeNumber === (gridNumber) ? "activated " : "") + (this.state.mapArray[gridNumber].hasEnemy ? "enemy" : "")} id={gridNumber++}>{abox}</div>
       )}
     </div>
     );
