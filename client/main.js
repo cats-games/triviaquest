f7d2c65a3a71043443c34a9c66d87696ca0ba6e3
Maureen E. Mirville <mirvil7130@aol.com>
2016-12-10 15:43:01 -0800

    implemented browserify

--- client/main.js
+++ client/main.js
@@ -1,15 +1,23 @@
 'use strict';
 
+console.log('main.js file recognized');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Grid from './src/components/Grid.jsx';
-/*import Gameinfo from './src/components/Gameinfo.jsx';
+import Gameinfo from './src/components/Gameinfo.jsx';
 import Textfield from './src/components/Textfield.jsx';
+<<<<<<< 85cd861f2e5f39f82150f87e38cf9e9b0a076235:client/main.js
 */
 console.log('main.js file recognized');
+=======
+>>>>>>> implemented browserify:client/main.jsx
 
+function main() {
+  return;
+}
 
+<<<<<<< 85cd861f2e5f39f82150f87e38cf9e9b0a076235:client/main.js
 function main() {
   return;
 }
@@ -27,6 +35,22 @@ const element = (
 
 console.log(element);
 
+=======
+const element = (
+  <div>
+  <h1>
+    REACT
+  </h1>
+  <nav>LUXURY NAV</nav>
+  <Grid />
+  <Gameinfo />
+  <Textfield />
+  </div>
+);
+
+console.log(element);
+
+>>>>>>> implemented browserify:client/main.jsx
 ReactDOM.render(
   element,
   document.getElementById('main')
--- client/src/components/Textfield.jsx
+++ client/src/components/Textfield.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 var Textfield = ({}) => (
   <div id="textfield" contenteditable="true">
   <form>
@@ -11,4 +14,4 @@ var Textfield = ({}) => (
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.Textfield = Textfield;
+export default Textfield;
