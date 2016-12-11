74501145c6e2d6d12a5491d09710d691b552a313
Maureen E. Mirville <mirvil7130@aol.com>
2016-12-11 01:24:33 -0800

    corrected test variables, test 2 pending

--- client/tests/e2e/grid.js
+++ client/tests/e2e/grid.js
@@ -27,32 +27,32 @@ describe('Grid test', function() {
       .assert.visible('#grid')
       .assert.visible('#grid .gridbox')
       .assert.visible('#space-1')
-      .assert.visible('#space-1.activated')
+      .assert.visible('#space-1.gridbox.player')
       .assert.visible('#space-25')
-      .assert.visible('#grid .activated.gridbox')
+      //.assert.visible('#grid .activated.gridbox')
       .expect.element('#answer').to.be.an('input');
   });
-
+  //this test below might be a problem as there may be an enemy in space-2
   it('should move the player when the navigation keys are pressed', function(client) {
     client.keys("l", function() {
       client.pause(1000);
-      client.assert.elementPresent('#space-2.activated');
-      client.assert.elementNotPresent('#space-1.activated');
+      client.assert.elementPresent('#space-2.gridbox.player');
+      client.assert.elementNotPresent('#space-1.gridbox.player');
     });
   });
 
   it('should not move player character while answering a question', function(client) {
     client.keys("llllljkkkkkjllllljkkkkk", function() {
       // Check that we are on a question.
-      client.assert.elementPresent('.gridbox.activated.enemy');
+      client.assert.elementPresent('.gridbox.enemy');
       client.expect.element('#gameinfo').to.have.value.which.matches(/[A-z]+.*/);
-      client.element('css selector', '.gridbox.activated.enemy', function(res) {
+      client.element('css selector', '.gridbox.enemy', function(res) {
         client.elementIdAttribute(res.value.ELEMENT, 'id', function(id) {
           // Save current position.
           var currentId = id.value;
           // Send a bunch of keystrokes.
           client.keys("llllljkkkkkjllllljkkkkk", function() {
-            client.element('css selector', '.gridbox.activated.enemy', function(res) {
+            client.element('css selector', '.gridbox.enemy', function(res) {
               client.elementIdAttribute(res.value.ELEMENT, 'id', function(id) {
                 // Verify that user is still in the same position after attempting to move player character.
                 // Because we are still on a question, we are expecting the player character not to move.
@@ -69,15 +69,15 @@ describe('Grid test', function() {
      client
        .url('http://localhost:8000')
        .waitForElementVisible('body', 1000);
-     client.assert.elementPresent('#space-1.activated');
+     client.assert.elementPresent('#space-1.gridbox.player');
      client.element('css selector', '#answer', function(res) {
        client.elementIdClick(res.value.ELEMENT, function() {
          client.keys("llllljkkkkkjllllljkkkkk", function() {
-           client.assert.elementPresent('#space-1.activated');
+           client.assert.elementPresent('#space-1.gridbox.player');
            client.element('css selector', '#grid', function(res) {
              client.elementIdClick(res.value.ELEMENT, function() {
                client.keys("llllljkkkkkjllllljkkkkk", function() {
-                 client.assert.elementNotPresent('#space-1.activated');
+                 client.assert.elementNotPresent('#space-1.gridbox.player');
                });
              });
            });
@@ -90,10 +90,10 @@ describe('Grid test', function() {
     client.url('http://localhost:8000').waitForElementVisible('body', 1000);
     // Move the character.
     client.keys("llllljkkkkkjllllljkkkkk", function() {
-      client.assert.elementNotPresent('#space-1.activated');
+      client.assert.elementNotPresent('#space-1.gridbox.player');
       // Ensure we are on a enemy square.
-      client.assert.elementPresent('.gridbox.activated.enemy');
-      client.element('css selector', '.gridbox.activated.enemy', function(res) {
+      client.assert.elementPresent('.gridbox.enemy');
+      client.element('css selector', '.gridbox.enemy', function(res) {
         client.elementIdAttribute(res.value.ELEMENT, 'id', function(id) {
           // Save current position.
           var currentId = id.value;
