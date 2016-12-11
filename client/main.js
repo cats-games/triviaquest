02d47c7e9d123e1ce55eaf8d7eaa0e72b2bdd67f
Maureen E. Mirville <mirvil7130@aol.com>
2016-12-10 16:10:24 -0800

    merge

--- client/main.js
+++ client/main.js
@@ -7,17 +7,7 @@ import ReactDOM from 'react-dom';
 import Grid from './src/components/Grid.jsx';
 import Gameinfo from './src/components/Gameinfo.jsx';
 import Textfield from './src/components/Textfield.jsx';
-<<<<<<< 85cd861f2e5f39f82150f87e38cf9e9b0a076235:client/main.js
-*/
-console.log('main.js file recognized');
-=======
->>>>>>> implemented browserify:client/main.jsx
 
-function main() {
-  return;
-}
-
-<<<<<<< 85cd861f2e5f39f82150f87e38cf9e9b0a076235:client/main.js
 function main() {
   return;
 }
@@ -33,24 +23,6 @@ const element = (
   </div>
 );
 
-console.log(element);
-
-=======
-const element = (
-  <div>
-  <h1>
-    REACT
-  </h1>
-  <nav>LUXURY NAV</nav>
-  <Grid />
-  <Gameinfo />
-  <Textfield />
-  </div>
-);
-
-console.log(element);
-
->>>>>>> implemented browserify:client/main.jsx
 ReactDOM.render(
   element,
   document.getElementById('main')
