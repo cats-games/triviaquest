c5b06b7c6e8fdc3401f99f2bd325f2d68c71a516
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-15 15:44:43 -0800

    User profile front end built

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -196,15 +196,15 @@ class App extends React.Component {
     // Render the gameboard, gameinfo, and text input field
     return (
       <div id="app">
-      <AppBar
-        title="It's a Game!"
-        showMenuIconButton={false}
-        iconElementRight={<div className="right-icon"><span className="github-name">{this.state.profile ? this.state.profile.name : ''}</span><a href="#" onClick={this.swapProfileView.bind(this)}><Avatar src={this.state.profile ? this.state.profile.picture : 'Profile'} size={35} backgroundColor='rgba(0,0,0,0)' /></a></div>}
-      />
+        <AppBar
+          title="It's a Game!"
+          showMenuIconButton={false}
+          iconElementRight={<div className="right-icon"><span className="github-name">{this.state.profile ? this.state.profile.name : ''}</span><a href="#" onClick={this.swapProfileView.bind(this)}><Avatar src={this.state.profile ? this.state.profile.picture : 'Profile'} size={35} backgroundColor='rgba(0,0,0,0)' /></a></div>}
+        />
 
         <div className= "game-display">
           <PlayerStatus health={_health} id="heart-display" />
-          {this.state.showPlayerProfile ? (<UserProfile />) : (<Grid grid={_grid} />)}
+          {this.state.showPlayerProfile ? (<UserProfile state={this.state} highScores={[100, 100, 100, 100, 100, 100, 100, 100, 100, 100]} />) : (<Grid grid={_grid} />)}
           <Gameinfo id="gameinfo" gameInfoText={gameInfoText}/>
           <Textfield checkAnswer={this.checkAnswer.bind(this)}/>
           <GameOver actions={this.actions} health={_health}/>
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,11 +1,55 @@
+'use strict'
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
-var UserProfile = () => {
-
+var UserProfile = ({state, highScores}) => {
+  const styles = {
+    h3: {
+      marginTop: 20,
+      fontWeight: 400,
+    },
+    block: {
+      display: 'flex',
+    },
+    block2: {
+      margin: 10,
+    },
+    pre: {
+      overflow: 'hidden', // Fix a scrolling issue on iOS.
+    },
+  };
+  // Want to make the avatar img change when player changes charactors...
   return (
     <div id= "user-profile">
-      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      <div id= "avatar">
+        <div>
+          <img src="../../img/p1_stand.png"></img>
+        </div>
+        <div>
+          <span className= "github-name">{state.profile ? state.profile.name : ''}</span>
+        </div>
+      </div>
+      <div id= "curr-score-board">
+        <div className= "current-score">
+          Current Score: <span>One Million</span>
+        </div>
+        <div className= "current-world">
+          Current World: <span>Oz</span>
+        </div>
+      </div>
+      <div id= "high-score-board">
+        <div className= "high-score-header">High Scores</div>
+        <div className= "high-scores">
+          {highScores.map(val =>
+            <div className= "high-score">{val} on 12/24/1980</div>
+          )}
+        </div>
+      </div>
+      <div id="profile-footer">
+        <a href="#">Resume</a>
+        <a href="#">Quit</a>
+      </div>
     </div>
   );
 };
