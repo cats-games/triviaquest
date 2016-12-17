db70a67af2e560d9a373b13d212fd35133309de1
Charleen Fei <fei.charleen@gmail.com>
2016-12-18 00:25:52 +0100

    coin flip with drop

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -294,10 +294,9 @@ class App extends React.Component {
         <div className= "game-display">
           <PlayerStatus health={_health} id="heart-display" />
           <Grid grid={_grid} state={this.state} />
-          {this.state.showPlayerProfile ? (<UserProfile state={this.state} swapProfileView={this.swapProfileView.bind(this)} logout={this.logout.bind(this)} />) : ''}
           <Gameinfo id="gameinfo" gameInfoText={gameInfoText}/>
           <Textfield state={this.state} checkAnswer={this.checkAnswer.bind(this)}/>
-          <img id="draggable" class="ui-widget-content" src="../../img/bitcoin.png" height="50" width="80" className={this.state.showPlayerProfile ? 'hidden' : ''}></img>
+          <img id="draggable" class="ui-widget-content" src="../../img/bitcoin.png" className={this.state.showPlayerProfile ? 'hidden' : ''}></img>
           <GameOver actions={this.actions} health={_health}/>
 
         </div>
--- client/ui.js
+++ client/ui.js
@@ -2,11 +2,11 @@
  * Created by charleen on 12/17/16.
  */
 $( function() {
-    $( "#draggable" ).draggable().css({'top': 35, 'left' : 20});
+    $( "#draggable" ).draggable().css({'height':60, 'width':80, 'top': 45, 'left' : 20});
     $( "#jukebox" ).droppable({
         drop: function() {
-            $("#draggable").hide("drop", {direction: "down"}, "slow", function(){
-                $(this).css({'top': 35, 'left' : 20}).fadeIn()
+            $("#draggable").css({'height':80, 'width':80}).hide("drop", {direction: "down"}, "slow", function(){
+                $(this).css({'height':60, 'width':80, 'top': 45, 'left' : 20}).fadeIn()
             });
         }
     })
