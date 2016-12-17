23a7ca7e95cc731bd6314726bc5cb35541919a97
TreTuna <tretuna@gmail.com>
2016-12-17 13:23:53 -0500

    Addeded BG soudns to sound component

--- client/src/components/Sounds.js
+++ client/src/components/Sounds.js
@@ -83,14 +83,26 @@ sounds.fx = {
 };
 
 sounds.bg = {
-  1: null,
-  2: null,
-  3: null,
-  4: null,
-  5: null,
-  6: null,
-  7: null,
-  8: null,
-  9: null,
-  10: null
+  1: '../../sounds/music/Rolemusic_-_01_-_A_ninja_among_culturachippers.mp3',
+  2: '../../sounds/music/Rolemusic_-_01_-_Bacterial_love.mp3',
+  3: '../../sounds/music/Rolemusic_-_01_-_Shipwreck_In_The_Pacific_Ocean.mp3',
+  4: '../../sounds/music/Rolemusic_-_01_-_Spell.mp3',
+  5: '../../sounds/music/Rolemusic_-_02_-_Ants.mp3',
+  6: '../../sounds/music/Rolemusic_-_02_-_ayVentura.mp3',
+  7: '../../sounds/music/Rolemusic_-_02_-_Chasing_The_Port_Chains.mp3',
+  8: '../../sounds/music/Rolemusic_-_02_-_Leafless_Quince_Tree.mp3',
+  9: '../../sounds/music/Rolemusic_-_03_-_Another_beek_beep_beer_please.mp3',
+  10: '../../sounds/music/Rolemusic_-_03_-_Crush_At_Maracas_Bar.mp3',
+  11: '../../sounds/music/Rolemusic_-_03_-_Python.mp3',
+  12: '../../sounds/music/Rolemusic_-_03_-_Straw_Fields.mp3',
+  13: '../../sounds/music/Rolemusic_-_04_-_Scape_from_the_city.mp3',
+  14: '../../sounds/music/Rolemusic_-_04_-_The_Pirate_And_The_Dancer.mp3',
+  15: '../../sounds/music/Rolemusic_-_04_-_Yellow_Dust.mp3',
+  16: '../../sounds/music/Rolemusic_-_05_-_Death_on_the_battlefield.mp3',
+  17: '../../sounds/music/Rolemusic_-_05_-_Poppies.mp3',
+  18: '../../sounds/music/Rolemusic_-_05_-_She_Is_My_Best_Treasure.mp3',
+  19: '../../sounds/music/Rolemusic_-_06_-_Donsi_Killed_la_rioja_star_Familiar_live.mp3',
+  20: '../../sounds/music/Rolemusic_-_06_-_He_Plays_Me_The_Best_Rhythms.mp3',
+  21: '../../sounds/music/Rolemusic_-_07_-_Beach_Wedding_Dance.mp3',
+  22: '../../sounds/music/Rolemusic_-_07_-_l3go.mp3'
 };
