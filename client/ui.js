680f3d42f0089393f3a31b9fc678cec8f54978d3
Charleen Fei <fei.charleen@gmail.com>
2016-12-18 00:19:21 +0100

    repositioned coin

--- client/ui.js
+++ client/ui.js
@@ -5,7 +5,9 @@ $( function() {
     $( "#draggable" ).draggable().css({'top': 35, 'left' : 20});
     $( "#jukebox" ).droppable({
         drop: function() {
-            console.log( "dropped" );
+            $("#draggable").hide("drop", {direction: "down"}, "slow", function(){
+                $(this).css({'top': 35, 'left' : 20}).fadeIn()
+            });
         }
     })
 } );
